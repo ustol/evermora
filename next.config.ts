d10a2dfc98ca8006a43b3b33eab8b57b20acb718
adolfokrah <adolfokrah@gmail.com>
2026-07-30 17:47:38 +0200

    fix: updates

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,7 @@ const nextConfig: NextConfig = {
   },
   allowedDevOrigins: [
     "localhost",
-    "ta-01kysh6snzwchh7e7t73j7xets-3000-1fbrb7wojjjzllulsqnu8hsrp.w.modal.host",
+    "https://akornafa.com",
   ],
 };
 
